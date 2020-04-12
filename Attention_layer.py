e708db89bf09f5d2a95dff99f6b07bdbf0974de8
song276795186 <3170104953@zju.edu.cn>
2020-04-12 15:49:56 +0800

    "commit 3 -- 0412--15:50"

--- Attention_layer.py
+++ Attention_layer.py
@@ -23,5 +23,25 @@ def subsequent_mask(size):
             [0., 0., 1., 1.],
             [0., 0., 0., 1.],
             [0., 0., 0., 0.]]])
-
     """
+    attn_shape = (1,size,size)
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype("uint8")
+    return torch.from_numpy(subsequent_mask) == 0
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)  #scaling factor d_k
+    #the fuction of the scaling is to prevent the softmax function has gradient vanish problem
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    """
+    key.transpose(-2,-1) 即将最后两个维度进行换轴， 例如将一个(4,3,2)的tensor转化为一个(4,2,3)的tensor
+    """
+    if mask is None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
